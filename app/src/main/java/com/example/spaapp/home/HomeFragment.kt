89386e141b72c958a4c36601995332b03fcd11c3
ItzTorres <torresitze1996@gmail.com>
2023-05-21 01:03:29 -0600

    Add images, icons and home view

---
+++ app/src/main/java/com/example/spaapp/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+/*
+ * HomeFragment.kt
+ * Created by Itz Torres on 21/5/23 01:42
+ * Copyright. All rights reserved.
+ */
+
+package com.example.spaapp.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment() {
+}
